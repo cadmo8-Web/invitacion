e4d14481331b9626ceda5c0dee14a2977f69d613
cadmo8-Web <cadmo8@gmail.com>
2026-07-31 14:41:19 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 function showTime() {
 	document.getElementById('currentTime').innerHTML = new Date().toUTCString();
 }
@@ -5,7 +6,7 @@ showTime();
 setInterval(function () {
 	showTime();
 }, 1000);
-
+*/
 // ---------- Música de fondo ----------
 const bgMusic = document.getElementById('bg-music');
 const musicToggle = document.getElementById('music-toggle');
